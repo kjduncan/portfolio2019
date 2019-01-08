1c38e118137a3fc3b9cc4ca59b82e0e69d227d3e
kjduncan <katelynduncan@Katelyns-MacBook-Pro.local>
2019-01-08 16:12:46 -0500

    project page and sidedoor content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,25 @@ import veritas from './images/veritas.png';
 import transport from './images/knightBus.png';
 import credit from './images/creditfreezeform1.jpg';
 import _ from 'lodash';
+import sideDoorLg from './images/sdfull.png';
+import propertySearch from './images/summaryOne.png';
+import propertyProfile from './images/propertyProfileL.png';
+import filter from './images/filter.png';
+import siteMap from './images/sitemap.png';
+import sdWire from './images/sdwire.png';
+import sdStyle from './images/sdStyle.png';
+import sdElements from './images/sdComponent.png';
+import sdBuy from './images/sdBuy.png';
+import sdCust from './images/sdCust.png';
+import sdMobileCalc from './images/sdMobileCalc.png';
+import sdMobileProfile from './images/sdMobileProfile.png';
+import sdMobileS from './images/sdMobileS.png';
+import sdNegotiate from './images/sdNegotiate.png';
+import sdSave from './images/sdSave.png';
+import sdSell from './images/sdSell.png';
+import sdShow from './images/sdShow.png';
+import sdSimple from './images/sdSimple.png';
+import sdDash from './images/sdDash.png';
 
 
 let projects = [
@@ -17,9 +36,26 @@ let projects = [
     title: 'SideDoor Inc',
     slug: 'sidedoor',
     attributes: {
-        description: 'An online marketplace for all things realestate',
+        description: 'An online marketplace for all things real estate',
         type: 'UX | UI | Design | Frontend Code',
-        imgSrc: sideDoor
+        imgSrc: sideDoor,
+        imgTop: sideDoorLg,
+        summary: 'The ultimate goal of SideDoor was to empower users in the real estate market to complete transactions on their own, ultimately saving around 6% of their home value. My role was to design and develop the UI alongside one backend developer. We used Bootstrap, LESS, CakePhp, Illustrator, and Photoshop.',
+        summaryOne: propertySearch,
+        summaryTwo: filter,
+        summaryThree: propertyProfile,
+        process: 'We started with key informational pages and a very light version of the property search page, creating a general sitemap to determine user flow. I would begin each page design with a lofi sketch of the layout, then move to digital wireframes. Finally, I would integrate content, brand colors, and UI details for final review and testing. Once designs were finalized, I began building the frontend of each page, passing them along to the backend before pushing to production.',
+        processOne: siteMap,
+        processTwo: sdStyle,
+        processThree: sdWire,
+        processFour: sdElements,
+        processSecondary: 'We created a living style guide on the web for this project from which we could pull components for our project. I started by creating a styleguide in illustrator that included typography, iconography, image sizes, and UI specifications. We then built it on the web and utilized these components throughout the project. ',
+        result: 'The result was an easy-to-use platform that effectively enabled users to buy and sell homes and to be connected with helpful resources throughout the process. The SideDoor user base increased consistently after implementing these changes and continued to grow over the course of the following year. ',
+        resultOne: sdCust,
+        resultTwo: sdShow,
+        resultThree: sdDash,
+        resultFour: sdBuy
+
     }
   },
   {
--- my-app/src/pages/project.js
+++ my-app/src/pages/project.js
@@ -1,11 +1,154 @@
 import React, { Component } from 'react';
+import mail from '../images/mail.svg';
+import linkedIn from '../images/linkedin.svg';
+import github from '../images/github.svg';
 
 class Project extends Component {
   render() {
     return (
       <div className="Project">
-        <h1>{this.props.project.title}</h1>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-7 is-offset-5 ">
+              <ul className="menu">
+                <li>
+                  <a href="">Resume</a>
+                </li>
+                <li>
+                  <a href="">About</a>
+                </li>
+                <li>
+                  <a className="nav-home" href="">Katelyn Duncan</a>
+                </li>
+              </ul>
+            </div>
+            <div className="column">
+              <div className="project-title">
+                <h1>{this.props.project.title}</h1>
+                <h3> {this.props.project.attributes.type}</h3>
+              </div>
+              <div className="project-description-lg column is-6 is-offset-3">
+                <p>{this.props.project.attributes.description}</p>
+              </div>
+              <img className="column is-8 is-offset-2" src={this.props.project.attributes.imgTop}/>
+              <div className="project-title">
+                <h1>Project Summary</h1>
+              </div>
+              <div className="column is-8 is-offset-2">
+                <p className="summary">{this.props.project.attributes.summary}</p>
+              </div>
+              <div className="columns">
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.summaryOne}/>
+                  <img src={this.props.project.attributes.summaryTwo}/>
+                </div>
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.summaryThree}/>
+                </div>
+              </div>
+              <div className="project-title">
+                <h1>Process</h1>
+              </div>
+              <div className="column is-8 is-offset-2">
+                <p className="summary">{this.props.project.attributes.process}</p>
+              </div>
+              <div className="columns">
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.processOne}/>
+                </div>
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.processTwo}/>
+                </div>
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.processThree}/>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-7 process-image">
+                  <img src={this.props.project.attributes.processFour}/>
+                </div>
+                <div className="column is-4">
+                  <p className="summary processSecondary">{this.props.project.attributes.processSecondary}</p>
+                </div>
 
+              </div>
+              <div className="project-title">
+                <h1>Result</h1>
+              </div>
+              <div className="column is-8 is-offset-2">
+                <p className="summary">{this.props.project.attributes.result}</p>
+              </div>
+              <img className="process-image" src={this.props.project.attributes.imgSrc}/>
+              <div className="columns">
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.resultOne}/>
+                </div>
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.resultTwo}/>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-6">
+                  <img className="process-image" src={this.props.project.attributes.resultThree}/>
+                </div>
+                <div className="column is-6">
+                  <img className="process-image" src={this.props.project.attributes.resultFour}/>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.resultFive}/>
+                </div>
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.resultSix}/>
+                </div>
+                <div className="column is-4 process-image">
+                  <img src={this.props.project.attributes.resultSeven}/>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.resultEight}/>
+                </div>
+                <div className="column is-6 process-image">
+                  <img src={this.props.project.attributes.resultNine}/>
+                </div>
+              </div>
+
+
+            </div>
+
+          </div>
+          </div>
+          <div className="columns footer">
+            <div className="is-two-fifths column">
+              <div className="landing-info">
+                <a className="nav-home" href="">Katelyn Duncan</a>
+                <ul className="social">
+                  <li>
+                    <a href=""><img src={mail}/></a>
+                  </li>
+                  <li>
+                    <a href=""><img src={github}/></a>
+                  </li>
+                  <li>
+                    <a href=""><img src={linkedIn}/></a>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+            <div className="footer-solid column is-three-fifths">
+              <ul className="menu">
+                <li>
+                  <a href="">Resume</a>
+                </li>
+                <li>
+                  <a href="">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
       </div>
     );
   }
